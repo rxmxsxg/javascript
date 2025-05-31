d73e82eccbd591350b7cc7f65c8526960d2e2753
rameshgongura <rameshgongura@gmail.com>
2025-05-31 08:49:30 +0530

    data types added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # javascript
+
 javascript practice
+
+topics
+
+# data types
+
+number -> 1,2.2
+string -> "", ''
+boolean -> true, flase
+arrays -> []
+objects -> {}
+
+arrays comes under object type that means typeof array is object
+
+# control statements
+
+# communication
---
+++ datatypes.js
@@ -0,0 +1,22 @@
+let isNum1 = 1
+console.log(typeof isNum1)
+let isNum2 = 2.2
+console.log(typeof isNum2)
+
+let isString1 = "ramesh"
+let isString2 = "hyd"
+console.log(typeof isString1)
+console.log(typeof isString2)
+
+let isTrue = true
+let isFalse = false
+
+console.log(typeof isTrue)
+console.log(typeof isFalse)
+
+let isArray = ["a", "b"]
+
+console.log(typeof isArray)
+
+let isObj = {}
+console.log(typeof isObj)
